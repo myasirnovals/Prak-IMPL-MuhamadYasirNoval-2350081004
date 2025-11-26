c0a7ab938381a13b44a77db7910238b6bf503afb
myasirnovalstudent <myasirnoval.student@gmail.com>
2025-11-26 07:42:21 +0700

    bug fix

--- Modul3/LatihanISP/src/sesudah/Motorcycle.java
+++ Modul3/LatihanISP/src/sesudah/Motorcycle.java
@@ -3,16 +3,16 @@ package sesudah;
 public class Motorcycle implements VehicleInterface {
     @Override
     public void drive() {
-
+        System.out.println("driving");
     }
 
     @Override
     public void stop() {
-
+        System.out.println("stop");
     }
 
     @Override
     public void refuel() {
-
+        System.out.println("refueling");
     }
 }
