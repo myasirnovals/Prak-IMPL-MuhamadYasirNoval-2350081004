52f1bade27095fd1023f7a26acfb0b9584cb444c
myasirnovalstudent <myasirnoval.student@gmail.com>
2025-11-27 13:40:59 +0700

    latihan Use Pronounceable Names

---
+++ Modul5/UsePronounceableNames/src/sesudah/Customer.java
@@ -0,0 +1,11 @@
+package sesudah;
+
+public class Customer {
+    private String id;
+    private String nama;
+    private String alamat;
+    private String no_hp;
+    private String email;
+    private String kode_pos;
+    private String kota;
+}
